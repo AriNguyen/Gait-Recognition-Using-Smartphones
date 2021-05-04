029a5a7776b07fda35118c58f9417d358c2d1dfc
AriNguyen3 <quynhnhu3132000@gmail.com>
2021-05-03 23:16:57 -0400

    add code/utils.py

---
+++ code/utils.py
@@ -0,0 +1,9 @@
+def one_hot(y_):
+    """
+    Function to encode output labels from number indexes.
+
+    E.g.: [[5], [0], [3]] --> [[0, 0, 0, 0, 0, 1], [1, 0, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0]]
+    """
+    y_ = y_.reshape(len(y_))
+    n_values = int(np.max(y_)) + 1
+    return np.eye(n_values)[np.array(y_, dtype=np.int32)]  # Returns FLOATS
