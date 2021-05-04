cc16abce36012d47f3c9db207b8ab0900592f346
AriNguyen3 <quynhnhu3132000@gmail.com>
2021-05-03 23:14:31 -0400

    add constants.py file to store dataset paths

---
+++ code/constants.py
@@ -0,0 +1,12 @@
+import os
+
+DATA_FOLDER_PATH = 'data/'
+
+DATASET_1 = os.path.join(DATA_FOLDER_PATH, 'Dataset #1/')
+DATASET_2 = os.path.join(DATA_FOLDER_PATH, 'Dataset #2/')
+DATASET_3 = os.path.join(DATA_FOLDER_PATH, 'Dataset #3/')
+DATASET_4 = os.path.join(DATA_FOLDER_PATH, 'Dataset #4/')
+DATASET_5 = os.path.join(DATA_FOLDER_PATH, 'Dataset #5/')
+DATASET_6 = os.path.join(DATA_FOLDER_PATH, 'Dataset #6/')
+DATASET_7 = os.path.join(DATA_FOLDER_PATH, 'Dataset #7/')
+DATASET_8 = os.path.join(DATA_FOLDER_PATH, 'Dataset #8/')
