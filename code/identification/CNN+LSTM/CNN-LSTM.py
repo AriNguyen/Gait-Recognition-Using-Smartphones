27a3f327c62fbb636a5ac16070bec8c4fff16f40
AriNguyen3 <quynhnhu3132000@gmail.com>
2021-06-03 08:54:32 -0400

    migrate to tensorflow 2, add early stoppings, callbacks, plotting, cross-validation

--- code/identification/CNN+LSTM/CNN-LSTM.py
+++ code/identification/CNN+LSTM/CNN-LSTM.py
@@ -6,26 +6,33 @@ from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 
 from utils import load_identification_data
-
-dataset_number = 1
+from constants import PROJECT_PATH, PLOT_SAVE_PATH
 
 # Training parameters
-batch_size = 512
-epochs = 100
-best_accuracy = 0
-feature_shape = 118
+DATASET_NUMBER = 1
+CHECKPOINT_PATH = os.path.join(PROJECT_PATH, 'code', "checkpoints/train")
 
 
 class Callback(keras.callbacks.Callback):
+    def __init__(self, monitor='acc', baseline=0.9):
+        self.monitor = monitor
+        self.baseline = baseline
+
     def on_epoch_end(self, epochs, logs={}):
-        if logs.get('loss') < 0.01:
-            print("\nReached 99% accuracy so cancelling training!")
+        if logs.get(self.monitor) >= self.baseline:
+            print("\nReached 99.9% validation accuracy so cancelling training!")
             self.model.stop_training = True
 
 
+def get_callbacks():
+    return [
+        Callback(monitor='acc', baseline=0.999),
+        tf.keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', min_delta=0.001, verbose=1, patience=20),
+    ]
+
+
 class Config:
-    """
-    define a class to store parameters,
+    """ A class to store parameters,
     the input should be feature mat of training and testing
 
     Note: it would be more interesting to use a HyperOpt search space:
@@ -48,7 +55,8 @@ class Config:
         # LSTM structure
         self.n_inputs = len(X_train[0][0])  # Features count is of 9: 3 * 3D sensors features over time
         self.n_hidden = 64  # nb of neurons inside the neural network
-        self.n_classes = feature_shape  # Final output classes
+        self.n_classes = 118  # Final output classes
+        self.dropout = 0.2
         self.W = {
             'hidden': tf.Variable(tf.random.normal([self.n_inputs, self.n_hidden])),
             'output': tf.Variable(tf.random.normal([self.n_hidden, self.n_classes]))
@@ -89,8 +97,8 @@ class LSTM_Network(tf.keras.Model):
         self.config = config
 
         # Define two stacked LSTM cells (two recurrent layers deep) with tensorflow
-        self.lstm_cell_1 = layers.LSTMCell(self.config.n_hidden, dropout=0.5)
-        self.lstm_cell_2 = layers.LSTMCell(self.config.n_hidden, dropout=0.5)
+        self.lstm_cell_1 = layers.LSTMCell(self.config.n_hidden, dropout=config.dropout)
+        self.lstm_cell_2 = layers.LSTMCell(self.config.n_hidden, dropout=config.dropout)
         self.stacked_lstm = layers.StackedRNNCells([self.lstm_cell_1, self.lstm_cell_2] * self.config.n_layers)
 
     def __call__(self, x):
@@ -141,72 +149,111 @@ class CNN_Network(tf.keras.Model):
         return x
 
 
-def last_full_connection_layer(lstm_output, cnn_output):
+def last_full_connection_layer(config, lstm_output, cnn_output):
     x = layers.concatenate([lstm_output, cnn_output], 1)
-    y = layers.Dense(feature_shape, activation='softmax')(x)
+    y = layers.Dense(config.n_classes, activation='softmax')(x)
     return y
 
 
-def experiment():
-    pass
+# def experiment():
+#     pass
+#
+#
+# lr_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(
+#     0.001,
+#     decay_steps=STEPS_PER_EPOCH*1000,
+#     decay_rate=1,
+#     staircase=False)
+#
+#
+# def get_optimizer():
+#   return tf.keras.optimizers.Adam(lr_schedule)
+
+
+# def compile_and_fit(model, name, optimizer=None, max_epochs=10000):
+#     if optimizer is None:
+#         optimizer = get_optimizer()
+#
+#     model.compile(optimizer=optimizer,
+#                   loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+#                   metrics=[
+#                       tf.keras.losses.BinaryCrossentropy(
+#                           from_logits=True, name='binary_crossentropy'),
+#                       'accuracy'])
+#
+#     model.summary()
+#
+#     history = model.fit(
+#         train_ds,
+#         steps_per_epoch=STEPS_PER_EPOCH,
+#         epochs=max_epochs,
+#         validation_data=validate_ds,
+#         callbacks=get_callbacks(name),
+#         verbose=0)
+#     return history
+
+
+def plot_model_result(model_history, model_result, config, saving_path=None):
+    # Retrieve a list of list results on training and test data
+    # sets for each training epoch
+    acc = model_history.history['acc']
+    val_acc = model_history.history['val_acc']
+    loss = model_history.history['loss']
+    val_loss = model_history.history['val_loss']
+    MAX_EPOCHS = range(len(acc))  # Get number of epochs
+
+    # Plot training and validation accuracy per epoch
+    plot_name = f'dataset#{str(DATASET_NUMBER)}_ACC_dropout{round(config.dropout, 2)}_batch{config.batch_size}' + \
+                f'_epochs{config.training_epochs}_lr{config.learning_rate}_testacc{round(model_result[1])}.jpg'
+    plt.plot(MAX_EPOCHS, acc, label='Training Accuracy')
+    plt.plot(MAX_EPOCHS, val_acc, label='Validation Accuracy')
+    plt.title('Training and validation accuracy')
+    plt.yticks(np.arange(0, 1.1, 0.1))
+    plt.xlabel("Epoch Number")
+    plt.ylabel("Accuracy")
+    plt.legend()
+    plt.savefig(os.path.join(PLOT_SAVE_PATH, plot_name))
+    plt.show()
+
+    # Plot training and validation loss per epoch
+    plot_name = f'dataset#{str(DATASET_NUMBER)}_LOSS_dropout{round(config.dropout, 2)}_batch{config.batch_size}' + \
+                f'_epochs{config.training_epochs}_lr{config.learning_rate}_testacc{round(model_result[1])}.jpg'
+    plt.plot(MAX_EPOCHS, loss, label='Training Loss')
+    plt.plot(MAX_EPOCHS, val_loss, label='Validation Loss')
+    plt.title('Training and validation loss')
+    plt.xlabel("Epoch Number")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.savefig(os.path.join(PLOT_SAVE_PATH, plot_name))
+    plt.show()
 
 
 if __name__ == '__main__':
-    # tf.compat.v1.disable_eager_execution()
     X = tf.keras.Input(shape=[128, 6], dtype='float32')
-    label = tf.keras.Input(shape=[feature_shape], dtype='float32')
 
-    # Load dataset
-    (X_train, X_test), (y_train, y_test) = load_identification_data(dataset_number=dataset_number)
+    (X_train, X_test), (y_train, y_test) = load_identification_data(dataset_number=DATASET_NUMBER)
 
-    # Reserve 10,000 samples for validation
-    x_val = X_train[-10000:]
-    y_val = y_train[-10000:]
-    x_train = X_train[:-10000]
-    y_train = y_train[:-10000]
-
-    # Initialize
-    config = Config(x_val, X_test)
+    config = Config(X_train, X_test)
     lstm_output = LSTM_Network(config)(X)
     cnn_output = CNN_Network()(X)
-    pred_Y = last_full_connection_layer(lstm_output, cnn_output)
+    pred_Y = last_full_connection_layer(config, lstm_output, cnn_output)
 
-    checkpoint_path = "./checkpoints/train"
-    earlyStopping = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=10)
-
+    opt = keras.optimizers.Adam(learning_rate=config.learning_rate)
     model = tf.keras.models.Model(inputs=X, outputs=pred_Y)
-    model.compile(optimizer='adam',
-                  loss=tf.keras.losses.CategoricalCrossentropy(),
-                  metrics='accuracy')
+    # model.save('../checkpoints/CNN_LSTM_weight.h5')
+    model.compile(optimizer=opt,
+                  loss='categorical_crossentropy',
+                  metrics='acc')
 
-    history = model.fit(x_train, y_train,
-                        validation_data=(x_val, y_val),
-                        batch_size=batch_size,
-                        epochs=epochs,
-                        callbacks=[Callback(), earlyStopping])
+    history = model.fit(X_train, y_train,
+                        validation_split=0.3,
+                        batch_size=config.batch_size,
+                        epochs=config.training_epochs,
+                        callbacks=get_callbacks(),
+                        shuffle=True)
 
-    # -----------------------------------------------------------
-    # Retrieve a list of list results on training and test data
-    # sets for each training epoch
-    # -----------------------------------------------------------
-    acc = history.history['accuracy']
-    val_acc = history.history['val_accuracy']
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
+    print("Evaluate on test data: ...")
+    results = model.evaluate(X_test, y_test, batch_size=128)
+    print("test loss, test acc:", results)
 
-    epochs = range(len(acc))  # Get number of epochs
-
-    # ------------------------------------------------
-    # Plot training and validation accuracy per epoch
-    # ------------------------------------------------
-    plt.plot(epochs, acc)
-    plt.plot(epochs, val_acc)
-    plt.title('Training and validation accuracy')
-    plt.figure()
-
-    # ------------------------------------------------
-    # Plot training and validation loss per epoch
-    # ------------------------------------------------
-    plt.plot(epochs, loss)
-    plt.plot(epochs, val_loss)
-    plt.title('Training and validation loss')
+    plot_model_result(history, results, config, saving_path=PLOT_SAVE_PATH)
