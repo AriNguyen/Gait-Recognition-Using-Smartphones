6c685b3f5b6836b2c18eb6744b375075617e5884
ARI NGUYEN <aringuyen@ARIs-MacBook-Pro.local>
2021-06-24 23:05:43 -0400

    code/identification/CNN/CNN.py migrate to TF 2.0

--- code/identification/CNN/CNN.py
+++ code/identification/CNN/CNN.py
@@ -1,172 +1,97 @@
-import tensorflow as tf
 import numpy as np
 import os
 import random
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True
+import utils
 
-def load_X(path):
-    X_signals = []
-    files = os.listdir(path)
-    files.sort(key=str.lower)
-    #['train_acc_x.txt', 'train_acc_y.txt', 'train_acc_z.txt', 'train_gyr_x.txt', 'train_gyr_y.txt', 'train_gyr_z.txt']
-    for my_file in files:
-        fileName = os.path.join(path,my_file)
-        file = open(fileName, 'r')
-        X_signals.append(
-            [np.array(cell, dtype=np.float32) for cell in [
-                row.strip().split(' ') for row in file
-            ]]
-        )
-        file.close()
-        #X_signals = 6*totalStepNum*128
-    X_signals = np.transpose(np.array(X_signals), (1, 0, 2))#(totalStepNum*6*128)
-    return X_signals.reshape(-1,6,128,1)#(totalStepNum*6*128*1)
-
-def load_y(y_path):
-    file = open(y_path, 'r')
-    # Read dataset from disk, dealing with text file's syntax
-    y_ = np.array(
-        [elem for elem in [
-            row.replace('  ', ' ').strip().split(' ') for row in file
-        ]],
-        dtype=np.int32
-    )
-    file.close()
-    # Substract 1 to each output class for friendly 0-based indexing
-    y_ = y_ - 1
-    #one_hot
-    y_ = y_.reshape(len(y_))
-    n_values = int(np.max(y_)) + 1
-    return np.eye(n_values)[np.array(y_, dtype=np.int32)]  # Returns FLOATS
-
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
-
-def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
-
-
+DATASET_NUMBER = 2
+training_epochs = 200
 batch_size = 512
-X_ = tf.placeholder(tf.float32, [None, 6, 128, 1],name='cnn_X')
-label_ = tf.placeholder(tf.float32, [None, 118],name='cnn_Y')
+learning_rate = 0.001
 
-#input shape [batch, in_height, in_width, in_channels]
-#kernel shape [filter_height, filter_width, in_channels, out_channels]
-'''
-	1*9
-	stride = 2 
-	padding
-	6*128->6*64*32
-'''
-W_conv1 = weight_variable([1, 9, 1, 32])
-b_conv1 = bias_variable([32])
-h_conv1 = tf.nn.relu(
-    tf.nn.conv2d(X_, W_conv1, strides=[1, 1, 2, 1], padding='SAME') + b_conv1)
-'''
-	pooling
-	6*64*32->6*32*32
-'''
-h_pool1 = tf.nn.max_pool(h_conv1, ksize=[1, 1, 2, 1], strides=[1, 1, 2, 1], padding='VALID')
-'''
-	1*3
-	stride = 1
-	6*32*32->6*32*64
-'''
-W_conv2 = weight_variable([1, 3, 32, 64])
-b_conv2 = bias_variable([64])
-h_conv2 = tf.nn.relu(
-    tf.nn.conv2d(h_pool1, W_conv2, strides=[1, 1, 1, 1], padding='SAME') + b_conv2)
-'''
-	1*3
-	stride = 1
-	padding
-	6*32*64->6*32*128
-'''
 
-W_conv3 = weight_variable([1, 3, 64, 128])
-b_conv3 = bias_variable([128])
-h_conv3 = tf.nn.relu(
-    tf.nn.conv2d(h_conv2, W_conv3, strides=[1, 1, 1, 1], padding='SAME') + b_conv3)
+def preprocess_X(X_signals):
+    X_signals = np.transpose(np.array(X_signals), (1, 0, 2))  # (totalStepNum*6*128)
+    return X_signals.reshape(-1, 6, 128, 1)  # (totalStepNum*6*128*1)
 
-'''
-'''
-'''
-	pooling
-	6*32*128->6*16*128
-'''
-h_pool2 = tf.nn.max_pool(h_conv3, ksize=[1, 1, 2, 1], strides=[1, 1, 2, 1],padding='VALID')
-'''
-	6*1
-	6*32*128->1*16*128
-'''
-W_conv4 = weight_variable([6, 1, 128, 128])
-b_conv4 = bias_variable([128])
-h_conv4 = tf.nn.relu(
-    tf.nn.conv2d(h_pool2, W_conv4, strides=[1, 1, 1, 1], padding='VALID') + b_conv4)
-'''
-	input flat 16*128=2048
-	output 20
-'''
-h_flat = tf.contrib.layers.flatten(h_conv4)
-cnn_output = tf.multiply(h_conv4,1,name='cnn_output')
-W_fc = weight_variable([2048, 118])
-b_fc = bias_variable([118])
-h_fc = tf.nn.softmax(tf.matmul(h_flat, W_fc) + b_fc)
 
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(label_ * tf.log(h_fc+1e-10), reduction_indices=[1]),name='cnn_loss')
-train_step = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(h_fc, 1), tf.argmax(label_, 1),name='cnn_pre_Y')
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32),name='cnn_accuracy')
+def preprocess_y(y):
+    # Substract 1 to each output class for friendly 0-based indexing
+    y = y - 1
+    # one_hot
+    y = y.reshape(len(y))
+    n_values = int(np.max(y)) + 1
+    return np.eye(n_values)[np.array(y, dtype=np.int32)]  # Returns FLOATS
 
-X_train = load_X('./data(118)/train/record')
-X_test = load_X('./data(118)/test/record')
 
-train_label = load_y('./data(118)/train/label.txt')
-test_label = load_y('./data(118)/test/label.txt')
+class CNN(tf.keras.Model):
+    def __init__(self):
+        super().__init__()
 
-saver = tf.train.Saver(max_to_keep=1)
+        self.model = tf.keras.Sequential([
+            layers.Conv2D(32, (1, 9), strides=(1, 2), padding='SAME', activation='relu', input_shape=(128, 6, 1)),
+            layers.MaxPool2D((1, 2), strides=(1, 2), padding='SAME'),
+            layers.Conv2D(64, (1, 3), strides=(1, 1), padding='SAME', activation='relu'),
+            layers.Conv2D(128, (1, 3), strides=(1, 1), padding='SAME', activation='relu'),
+            layers.MaxPool2D((1, 2), strides=(1, 2), padding='VALID'),
+            layers.Conv2D(128, (6, 1), strides=(1, 1), padding='VALID', activation='relu'),
+            layers.Flatten(),
+            layers.Dense(20, activation='softmax'),  # 20 or 119
+        ])
+        print(self.model.summary())
 
-sess = tf.InteractiveSession(config=config)
-sess.run(tf.global_variables_initializer())
+    def __call__(self, x):
+        x = tf.reshape(x, [-1, 128, 6, 1])
+        x = self.model(x)
+        return x
 
-if os.path.exists('./cnn_ckpt'):
-    saver.restore(sess,tf.train.latest_checkpoint('./cnn_ckpt/'))
 
-best_accuracy = 0
-f = open('./result_cnn.txt','w')
-for i in range(200):
-    l = len(train_label)
-    batch_idxs = int(l / batch_size)
-    index = list(range(l))
-    random.shuffle(index)
-    for idx in range(batch_idxs):
-        image_idx = X_train[index[idx * batch_size:(idx + 1) * batch_size]]
-        label_idx = train_label[index[idx * batch_size:(idx + 1) * batch_size]]
-        #print(start,end)
-        acc, loss, _ = sess.run([accuracy, cross_entropy, train_step], feed_dict={
-            X_: image_idx,
-            label_: label_idx
-        })
-        if idx % 100 == 0:
-            print(str(i) + 'the cross_entropy:', str(loss), 'train_accuracy:', str(acc))
-            f.write(str(i) + 'the cross_entropy:'+str(loss)+'train_accuracy:'+str(acc))
-        # Test completely at every epoch: calculate accuracy
-    accuracy_out, loss_out = sess.run(
-        [accuracy, cross_entropy],
-        feed_dict={
-            X_: X_test,
-            label_: test_label
-        }
-    )
-    if accuracy_out > best_accuracy:
-        saver.save(sess,'./cnn_ckpt/model')
-        best_accuracy = accuracy_out
-    print(str(i)+'--------------the cross_entropy:', str(loss_out), '-----------------------test_accuracy:', str(accuracy_out))
-    f.write(str(i)+'--------------the cross_entropy:'+str(loss_out)+'-----------------------test_accuracy:'+str(accuracy_out))
-print("best accuracy:"+str(best_accuracy))
-f.write("best accuracy:"+str(best_accuracy))
-f.close()
+class Callback(keras.callbacks.Callback):
+    def __init__(self, monitor='acc', baseline=0.9):
+        self.monitor = monitor
+        self.baseline = baseline
+
+    def on_epoch_end(self, epochs, logs={}):
+        if logs.get(self.monitor) >= self.baseline:
+            print("\nReached 99.9% validation accuracy so cancelling training!")
+            self.model.stop_training = True
+
+
+def get_callbacks():
+    return [
+        Callback(monitor='acc', baseline=0.999),
+        tf.keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', min_delta=0.001, verbose=1, patience=20),
+    ]
+
+
+if __name__ == '__main__':
+    X = tf.keras.Input(shape=[128, 6, 1], dtype='float32')
+
+    (X_train, X_test), (y_train, y_test) = utils.load_identification_data(dataset_number=DATASET_NUMBER,
+                                                                          preprocess_x_func=preprocess_X,
+                                                                          preprocess_y_func=preprocess_y)
+
+    cnn_output = CNN()(X)
+
+    opt = keras.optimizers.Adam(learning_rate=learning_rate)
+    model = tf.keras.models.Model(inputs=X, outputs=cnn_output)
+    # model.save('../checkpoints/CNN_LSTM_weight.h5')
+    model.compile(optimizer=opt,
+                  loss='categorical_crossentropy',
+                  metrics='acc')
+
+    history = model.fit(X_train, y_train,
+                        validation_split=0.3,
+                        batch_size=batch_size,
+                        epochs=training_epochs,
+                        callbacks=get_callbacks(),
+                        shuffle=True)
+
+    print("Evaluate on test data: ...")
+    results = model.evaluate(X_test, y_test, batch_size=128)
+    print("test loss, test acc:", results)
+
+    # plot_model_result(history, results, DATASET_NUMBER, configNone, saving_path=PLOT_SAVE_PATH)
